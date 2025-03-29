68c0fcb852c90efb524fe06ef8cd33f597107258
harshit09854 <harshitpokharna24@gmail.com>
2025-03-29 12:58:24 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 
+#!/bin/sh
+
+# Convert JOB_NAME to lowercase
 JOB_NAME_LOWER=$(echo "$JOB_NAME" | tr '[:upper:]' '[:lower:]')
 
+# Build Docker image with lowercase name
+docker build -t $JOB_NAME_LOWER:$BUILD_ID .
 
-docker build -t $JOB_NAME:$BUILD_ID .
+# Tagging with lowercase repository name
+docker tag $JOB_NAME_LOWER:$BUILD_ID harshit356/$JOB_NAME_LOWER:$BUILD_ID
+docker tag $JOB_NAME_LOWER:$BUILD_ID harshit356/$JOB_NAME_LOWER:latest
 
-docker tag $JOB_NAME:$BUILD_ID harshit356/$JOB_NAME:$BUILD_ID
+# Push to Docker Hub
+docker push harshit356/$JOB_NAME_LOWER:$BUILD_ID
+docker push harshit356/$JOB_NAME_LOWER:latest
 
-docker tag $JOB_NAME:$BUILD_ID harshit356/$JOB_NAME:latest
-
-docker push harshit356/$JOB_NAME:$BUILD_ID
-
-docker push harshit356/$JOB_NAME:latest
-
-docker rmi -f $JOB_NAME:$BUILD_ID harshit356/$JOB_NAME:$BUILD_ID harshit356/$JOB_NAME:latest
+# Clean up local images
+docker rmi -f $JOB_NAME_LOWER:$BUILD_ID harshit356/$JOB_NAME_LOWER:$BUILD_ID jacksneel/$JOB_NAME_LOWER:latest
